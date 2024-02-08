11af70b9e05ca57a9f0968577f786f22353bb603
palaniselvamsarathy <palaniselvamsarathy@gmail.com>
2024-02-08 12:10:15 +0530

    hash

--- Hash/two.py
+++ Hash/two.py
@@ -1,13 +1,10 @@
-stock_prices = []
+stock_prices = {}
 with open ("stock_prices.csv") as f:
     for line in f:
         tokens=line.split(',')
         day = tokens[0]
         price = float(tokens[1])
-        stock_prices.append([day,price])
+        stock_prices[day]=price
 
 print(stock_prices)
-
-for elem in stock_prices:
-    if elem[0]=="9 Mar":
-        print(elem[1])
+print(stock_prices['9 Mar'])
